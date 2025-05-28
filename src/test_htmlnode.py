6ad2997c650475962752b91fd11917a924817dbf
skamprogiannis <skamprogiannis@gmail.com>
2025-05-28 15:55:24 +0300

    feat: add HTMLNode class and tests for the class
    - Created htmlnode.py with HTMLNode class representing an HTML document
      tree node
    - Implemented __init__ with optional tag, value, children, and props
    - Added props_to_html() to render HTML attributes with leading spaces
    - Added __repr__ for readable object representation
    - Created unit tests for props_to_html and __repr__ in test_htmlnode.py

---
+++ src/test_htmlnode.py
@@ -0,0 +1,30 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+
+
+        def test_repr_eq(self):
+            child_node1 = HTMLNode()
+            child_node2 = HTMLNode()
+            p_node = HTMLNode("p", "I am a paragraph", [child_node1, child_node2])
+            self.assertEqual(repr(p_node), "HTMLNode(p, I am a paragraph, "
+            "[HTMLNode(None, None, None, None), HTMLNode(None, None, None, None)], None)")
+
+            self.assertEqual(repr(child_node1), repr(child_node2))
+
+        def test_props_eq(self):
+            span_node = HTMLNode("span", "the list of assert methods", None, {"class": "std std-ref"})
+            a_node = HTMLNode("a", None, [span_node], {"class": "reference internal", "href": "#assert-methods", "target": "_self"})
+
+            self.assertEqual(a_node.props_to_html(), ' class="reference internal" href="#assert-methods" target="_self"')
+            self.assertEqual(span_node.props_to_html(), ' class="std std-ref"')
+
+        def test_props_uneq(self):
+            span_node = HTMLNode("span", "the list of assert methods", None, {"class": "std std-ref"})
+            a_node = HTMLNode("a", None, [span_node], {"class": "reference internal", "href": "#assert-methods", "target": "_self"})
+            empty_node = HTMLNode()
+            
+            self.assertNotEqual(a_node.props_to_html(), span_node.props_to_html())
+            self.assertNotEqual(a_node.props_to_html(), empty_node.props_to_html())
